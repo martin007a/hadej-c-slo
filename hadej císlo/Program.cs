119029f4d900f42f25f13127040456f2eafeee3a
slavicek.martin <slaviekmartin@post.cz>
2021-11-16 11:30:09 +0100

    hotovo

--- hadej císlo/Program.cs
+++ hadej císlo/Program.cs
@@ -6,12 +6,38 @@ namespace hadej_císlo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hadej číslo!");
             
+            
+            Random random = new Random();
+            int hacislo = random.Next(1, 100);
+            int tipcislo;
+            bool neuhodl = true;
+           
+            Console.WriteLine("Hadání čísla");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Myslím si číslo od 1 do 100!");
+            while (neuhodl);
+            {
+                Console.WriteLine("Tipni si číslo: ");
+                tipcislo = int.Parse(Console.ReadLine());
+                
+                if (tipcislo > hacislo);
+                {
+                    Console.WriteLine("číslo je velké");
+                }
+                else if (tipcislo > hacislo);
+                {
+                    Console.WriteLine("číslo je malé");
+                }
+                else
+                {
+                    Console.WriteLine("Trefa");
+                    neuhodl = false;
+                }
+                
 
-
-
-
+            }
+            Console.ReadLine();
         }
     }
 }
