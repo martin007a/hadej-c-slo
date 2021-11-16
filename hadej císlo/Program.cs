0f80b315f0029f769b2ac8f3a55deef0f96f9ea6
slavicek.martin <slaviekmartin@post.cz>
2021-11-16 10:07:47 +0100

    Přidejte soubory projektu.

---
+++ hadej císlo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hadej_císlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hadej číslo!");
+            
+
+
+
+
+        }
+    }
+}
